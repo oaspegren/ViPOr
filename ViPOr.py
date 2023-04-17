73cadad7e11249a84520700f5a79ab94d47a9d04
oaspegren <65176033+oaspegren@users.noreply.github.com>
2023-04-17 12:36:29 -0400

    Add files via upload

---
+++ ViPOr.py
@@ -0,0 +1,14 @@
+# Main Website interface
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome to ViPOr!",
+    page_icon="🐍",
+)
+
+st.markdown("# Welcome to ViPOr: Visualizing POtentials and Orbits with Python!")
+
+st.markdown("### A teaching tool to understand how particles move in different galaxy potentials.")
+
+st.markdown("## Select an application from the sidebar to begin.")
