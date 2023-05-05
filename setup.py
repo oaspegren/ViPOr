6b29c30fe6cd4c13bbc02c3fc41c3e0f558f4b29
oaspegren <65176033+oaspegren@users.noreply.github.com>
2023-05-05 19:49:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setuptools.setup(
      author="Olivia Aspegren",
      author_email="olivia.aspegren@yale.edu",
      description="A teaching tool for understanding galaxy potentials and orbits",
-     packages=["ViPOr","ViPOr/pages"]
+     packages=["ViPOr","ViPOr/pages"],
+     install_requires=["streamlit", "numpy", "galpy", "matplotlib", "astropy", "numexpr"],
+     python_requires='>=3'
 )
 
-python_requires='>=3'
 
-install_requires=["streamlit", "numpy", "galpy", "matplotlib", "astropy", "numexpr"]
