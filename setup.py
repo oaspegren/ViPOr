5192b67cdfb7e1b580f99330c7c23847f0bbc191
oaspegren <65176033+oaspegren@users.noreply.github.com>
2023-05-05 21:38:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ import setuptools
 
 setuptools.setup(
      name="ViPOr",
-     version="0.1",
+     version="0.5",
      author="Olivia Aspegren",
      author_email="olivia.aspegren@yale.edu",
      description="A teaching tool for understanding galaxy potentials and orbits",
      packages=["ViPOr","ViPOr/pages"],
      install_requires=["streamlit", "numpy", "galpy", "matplotlib", "astropy", "numexpr"],
-     python_requires='>=3'
 )
 
