b395ea881cefa1a24c9812ba59c4347e61ea6232
oaspegren <65176033+oaspegren@users.noreply.github.com>
2023-05-08 09:34:55 -0400

    Update 6_A_Generic_Triaxial_Example.py

--- ViPOr/pages/6_A_Generic_Triaxial_Example.py
+++ ViPOr/pages/6_A_Generic_Triaxial_Example.py
@@ -12,14 +12,15 @@ from PlotPotentialandOrbit2D import plot_orbit_2D
 from PlotPotentialandOrbit3D import plot_orbit_3D
 
 st.markdown("## Looking at Orbits in a Power-law Triaxial Potential")
-st.markdown("We now look at an example of an triaxial potential. These are orbits created \
-	by elliptical galaxies, and are in general not symmetrical in any direction.")
+st.markdown("We now look at an example of an triaxial potential. These are orbits created by elliptical galaxies.")
 
+# give the general equation for the axisymmetric orbit
 st.markdown("The general equation for the density of a power-law triaxial distribution is:")
 st.latex(r'''\rho(r)= \text{amp} \, r^{-3} \left(\frac{r_1}{r}\right)^\alpha''')
 
 param_string = r"For this potential, you can modify the power-law exponent, $\alpha$."
 
+# print out the string above
 st.markdown(param_string)
 
 st.markdown(
@@ -30,19 +31,20 @@ st.markdown(
 st.markdown("NOTE: This may take a few moments to run and generate plots. If the application throws an error, that's because the parameters are not \
 	allowed — please select new ones.")
 
+# create sliders for the key parameter, length of time over which to integrate, radius and height above the galactic plane
 param = st.slider("Power-law Exponent, " + r"$\alpha$", min_value = 0.5, max_value = 5.0, step = 0.25)
-
 years = st.slider("Time (Gyr):", min_value = 0, max_value = 14)
 radius = st.slider("Set the initial distance from the galactic center:", min_value = 0.0, max_value = 50.0, step = 1.0)
 height = st.slider("Set the initial height from the galactic plane:", min_value = 0.0, max_value = 50.0, step = 1.0)
 
 pot_fxn_set = PowerTriaxialPotential(alpha = param)
 
-#st.pyplot(plt.imshow(plotPotentials(pot_fxn_set)))
-
+# get all the two-dimensional plots and data for the animations from plot_orbit_2D
 fig0, fig1, fig2, fig3, raw_html, density = plot_orbit_2D(pot_fxn_set, years, radius, height)
 
-st.markdown("This is what a density plot of the potential looks like.")
+st.markdown("Below is a plot of the potential for the Milky Way with the components selected, over each value of R and \
+		z. The darker regions are areas where the magnitude of the potential is higher — so we can see that the potential \
+		increases as the object gets closer to the center.")
 
 st.pyplot(density.get_figure())
 
@@ -56,40 +58,48 @@ st.markdown("The second is an **Radius vs. radial velocity** plot, which display
 	from the galactic center. We can track where the velocity is positive and where it is negative, which indicates the direction the particle orbits. Bound orbits will \
 	fluctuate between positive and negative, while unbound orbits will increase in radial velocity off into infinity.")
 
+# show the plot in R vs. vR coordinates
 st.pyplot(fig2, bbox_inches = "tight", pad_inches = 0.5)
 
 st.markdown("The next is an **RA vs. Dec** plot, which displays the path of the orbit in celestial coordinates. This is the path the particle takes within the sky.")
 
+# show the plot in right ascension vs. declination coordinates
 st.pyplot(fig1, bbox_inches = "tight", pad_inches = 0.5)
 
 st.markdown("Lastly, we show **the projection of the orbit into the x-y plane**, which shows the position and movement of the particle through the galactic plane.")
 
+# show the orbit in Cartesian coordinates 
 st.pyplot(fig3, bbox_inches = "tight", pad_inches = 0.5)
 
 st.markdown("Finally, there is an animation that displays the movement of the particle in the meridional plane — once again, its vertical height from the galactic plane \
 	at each radius.")
 
+# show the animation in two dimensions
 st.components.v1.html(raw_html, height = 800)
 
 st.markdown("We can also explore this orbit in 3 dimensions... ")
 
+# get all of the plots of this potential from plot_orbit_3D
 fig0, fig1, fig2, raw_html, density_3D = plot_orbit_3D(pot_fxn_set, years, radius, height)
 
 st.markdown("This first plot shows the orbit in **x, y and z coordinates**. With this we can see how the particle will move \
 	in three-dimensional space.")
 
+# show the Cartesian coordinates plot
 st.pyplot(fig0, bbox_inches = "tight", pad_inches = 0.5)
 
 st.markdown("This next plot shows the relationship between the **orbital radius, radial velocity and height from the disk plane**. \
 	This shows how the radial velocity of the particle changes with its position inside the distribution.")
 
+# show the R vs. vr vs. z plot
 st.pyplot(fig1, bbox_inches = "tight", pad_inches = 0.5)
 
 st.markdown("The last plot shows the relationship between the particle's **orbital radius, radial velocity and vertical velocity**. \
 	This plot shows how fast the particle is moving radially and vertically, depending on its distance from the center of the galaxy.")
 
+# show the R vs. vR vs. vz plot
 st.pyplot(fig2, bbox_inches = "tight", pad_inches = 0.5)
 
+# show the animation
 st.markdown("And now we display the 3D animation of the orbit.")
 st.components.v1.html(raw_html, height = 800)
-
