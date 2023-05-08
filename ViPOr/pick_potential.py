af8aeac53a9f10fc7dda0e853458b4290da26113
oaspegren <65176033+oaspegren@users.noreply.github.com>
2023-05-08 16:04:32 -0400

    Update pick_potential.py

--- ViPOr/pick_potential.py
+++ ViPOr/pick_potential.py
@@ -19,34 +19,36 @@ def pick_potential(pot_fxn, index):
 
 	Outputs: 
 	-------
-	parameters:
-	equation:
-	param1:
-	density_string:
-	latex:
-	min_value:
-	max_value:
-	step:
+	parameters: a string with the name of the parameter that the user may alter
+	equation: the equation, in latex, of either the mass distribution or the potential
+	param1: a string, which will be input into the slider, of the paramter of interest
+	density_string: another string, which either introduces the equation for the density or the potential
+	latex: the latex form of the mass density or the potential
+	min_value: the minimum value of the key parameter for the slider
+	max_value: the maximum value of the key parameter for the slider
+	step: the value of the steps for the slider
 	'''	
 
+	# list of the key parameters and equations in latex for each potential
 	mod_params = [r"$\alpha$, the power law exponent", r"$a$, the radius of the shell", r"$R$, the radius of the sphere", r"$b$, the scale parameter"]
 	eqns = [r'''\rho(r)= \frac{\text{amp}}{r^{3}} \left(\frac{r_1}{r}\right)^\alpha''', \
 	r'''\rho(r)= \frac{\text{amp}}{4\pi a^2} \delta(r-a)''', r'''\rho(r)= \rho_0''', r'''\Phi(R, z) = \frac{\text{amp}}{\sqrt{R^2 + z^2 + b^2}''']
 
-	pot_name_string = "You've selected the **"+pot_fxn+"**."
-
+	# selecting the string with the name of the potential and the latexed equation 
 	parameters = mod_params[index]
 	equation = eqns[index]
 
+	# if we're using something other than the plummer potential, then we have an equation for the mass density distribution
 	if pot_fxn != "Plummer Potential":
 		density_string = "The equation for the density of this distribution is:"
 		latex = equation
 
+	# if we're using the plummer potential, we only have the form of the potential, not of the mass density
 	else:
 		density_string = "This potential is characterized by the equation:"
 		latex = r'''\Phi(R,z) = - \frac{\text{amp}}{\sqrt{R^2 + z^2 + b^2}}'''
 
-
+	# for each potential, set the key parameter of interest, as well as its minimum, maximum and step values for the sliders
 	if pot_fxn == "Power Spherical Potential":
 		param1 = "Power Law Exponent, " + r"$\alpha$"
 		min_value = 0.0
@@ -88,6 +90,7 @@ def set_potential(pot_fxn, param):
 	pot_fxn_set: the initialized potential, with the key parameter set
 	'''
 
+	# for each potential, initialize the potential based on the value of the key parameter
 	if pot_fxn == "Power Spherical Potential":
 		pot_fxn_set = PowerSphericalPotential(alpha = param)
 
